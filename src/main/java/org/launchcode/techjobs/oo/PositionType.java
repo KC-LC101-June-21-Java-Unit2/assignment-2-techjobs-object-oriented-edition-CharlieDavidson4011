6be5f98e888442dceb386c065a3d31f8dc0abed3
Charlie Davidson <charliedavidson4011@gmail.com>
2021-11-29 14:17:44 -0600

    Location and PositionType DRY

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,32 +1,27 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
-
 public class PositionType extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    //private int id;
+    //private static int nextId = 1;
+    //private String value;
 
     public PositionType() {
-        id = nextId;
-        nextId++;
+    //    id = nextId;
+    //    nextId++;
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
+       // this();
+       // this.value = value;
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-    public String toString() {
-        return value;
-    }
+    //@Override
+    //public String toString() {
+    //    return value;
+    //}
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
@@ -36,21 +31,21 @@ public class PositionType extends JobField {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id;
+        return getId() == that.getId();
     }
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
+   // public int getId() {
+   //     return id;
+   // }
 
-    public String getValue() {
-        return value;
-    }
+   // public String getValue() {
+   //     return value;
+   // }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+    //public void setValue(String value) {
+    //this.value = value;
+    //}
 
 }
